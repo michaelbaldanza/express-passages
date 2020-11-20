397a9fb1962ceb2220fa40c6a6dad60f6979cfd4
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-11-20 12:15:56 -0500

    implement oauth and some routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express Passages' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] },
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/users',
+    failureRedirect : '/users',
+  },
+));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/users');
 });
 
 module.exports = router;
+
+//PICK UP FROM STEP 9 OF OAUTH AUTHENTICATION MODULE
